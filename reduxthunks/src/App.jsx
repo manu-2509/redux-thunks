f3e1a284c77159054e28cea5421394f9d03b9492
manu-2509 <maneshs111@gmail.com>
2022-05-10 21:57:28 +0530

    thunk

--- reduxthunks/src/App.jsx
+++ reduxthunks/src/App.jsx
@@ -5,9 +5,7 @@ import { Route, Routes } from 'react-router'
 import { Login } from './components/Login'
 import { Signup } from './components/Signup'
 import {UserData} from './components/Home'
-import { EditData } from './components/Edit'
-import { SuperAdmin } from './components/SuperAdmin'
-import { AllUsers } from './components/AllUsers'
+
 
 function App() {
 
@@ -18,9 +16,7 @@ function App() {
           <Route path="/Dashboard" element={<UserData />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
-          <Route path="/edit" element={<EditData/>}/>
-          <Route path="/SuperAdmin" element={<SuperAdmin/>}/>
-          <Route  path="/users" element={<AllUsers/>} />
+   
         </Routes>
     </div>
   )
